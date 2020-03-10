2e3bd3f134d87c475e750bd0ac9f3a0412b4d25b
Atanas Vihrogonov <vihrogonov@protonmail.com>
2020-03-10 21:10:51 +0000

    Get from External API

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 document.getElementById('button1').addEventListener('click', getText);
 document.getElementById('button2').addEventListener('click', getJson);
-document.getElementById('button3').addEventListener('click', getAPI);
+document.getElementById('button3').addEventListener('click', getExAPI);
 
 // Get Local Text File Data
 function getText() {
@@ -31,6 +31,28 @@ function getJson() {
       let output = '';
       data.forEach(function(post) {
         output += `<li>${post.title}</li>`;
+      });
+
+      document.getElementById('output').innerHTML = output;
+    })
+
+    .catch(function(err) {
+      console.log(err);
+    });
+}
+
+// Get From External API
+function getExAPI() {
+  fetch('https://api.github.com/users')
+    .then(function(res) {
+      return res.json();
+    })
+
+    .then(function(data) {
+      console.log(data);
+      let output = '';
+      data.forEach(function(user) {
+        output += `<li>${user.login}</li>`;
       });
 
       document.getElementById('output').innerHTML = output;
