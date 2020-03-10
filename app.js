fa03f0c3df5e4a51f0acea6c59f3e9f61c1c4d30
Atanas Vihrogonov <vihrogonov@protonmail.com>
2020-03-10 22:09:47 +0000

    Arrow function

--- app.js
+++ app.js
@@ -3,52 +3,104 @@ document.getElementById('button2').addEventListener('click', getJson);
 document.getElementById('button3').addEventListener('click', getExAPI);
 
 // Get Local Text File Data
+// function getText() {
+//   fetch('test.txt')
+//     .then(function(res) {
+//       return res.text();
+//     })
+
+//     .then(function(data) {
+//       //console.log(data);
+//       document.getElementById('output').innerHTML = data;
+//     })
+
+//     .catch(function(err) {
+//       console.log(err);
+//     });
+// }
+
 function getText() {
   fetch('test.txt')
-    .then(function(res) {
-      return res.text();
-    })
+    .then(res => res.text())
 
-    .then(function(data) {
+    .then(data => {
       //console.log(data);
       document.getElementById('output').innerHTML = data;
     })
 
-    .catch(function(err) {
-      console.log(err);
-    });
+    .catch(err => console.log(err));
 }
 
+
+
 // Get Local JSON File Data
+// function getJson() {
+//   fetch('post.json')
+//     .then(function(res) {
+//       return res.json();
+//     })
+
+//     .then(function(data) {
+//       console.log(data);
+//       let output = '';
+//       data.forEach(function(post) {
+//         output += `<li>${post.title}</li>`;
+//       });
+
+//       document.getElementById('output').innerHTML = output;
+//     })
+
+//     .catch(function(err) {
+//       console.log(err);
+//     });
+// }
+
 function getJson() {
   fetch('post.json')
-    .then(function(res) {
-      return res.json();
-    })
+    .then(res => res.json())
 
-    .then(function(data) {
+    .then(data => {
       console.log(data);
       let output = '';
       data.forEach(function(post) {
         output += `<li>${post.title}</li>`;
       });
-
+      
       document.getElementById('output').innerHTML = output;
     })
 
-    .catch(function(err) {
-      console.log(err);
-    });
+    .catch(err => console.log(err));
 }
 
+
+
 // Get From External API
+// function getExAPI() {
+//   fetch('https://api.github.com/users')
+//     .then(function(res) {
+//       return res.json();
+//     })
+
+//     .then(function(data) {
+//       console.log(data);
+//       let output = '';
+//       data.forEach(function(user) {
+//         output += `<li>${user.login}</li>`;
+//       });
+
+//       document.getElementById('output').innerHTML = output;
+//     })
+
+//     .catch(function(err) {
+//       console.log(err);
+//     });
+// }
+
 function getExAPI() {
   fetch('https://api.github.com/users')
-    .then(function(res) {
-      return res.json();
-    })
+    .then(res => res.json())
 
-    .then(function(data) {
+    .then(data => {
       console.log(data);
       let output = '';
       data.forEach(function(user) {
@@ -58,7 +110,5 @@ function getExAPI() {
       document.getElementById('output').innerHTML = output;
     })
 
-    .catch(function(err) {
-      console.log(err);
-    });
+    .catch(err => console.log(err));
 }
