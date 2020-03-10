5ae75ecee6f0ece110a1ea850398c8bf6b466ef1
Atanas Vihrogonov <vihrogonov@protonmail.com>
2020-03-10 21:02:11 +0000

    simple POST Request

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 document.getElementById('button1').addEventListener('click', getText);
+document.getElementById('button2').addEventListener('click', getJson);
+document.getElementById('button3').addEventListener('click', getAPI);
 
+// Get Local Text File Data
 function getText() {
   fetch('test.txt')
     .then(function(res) {
@@ -7,10 +10,33 @@ function getText() {
     })
 
     .then(function(data) {
-      console.log(data);
+      //console.log(data);
+      document.getElementById('output').innerHTML = data;
     })
 
     .catch(function(err) {
-      console.log(error);
+      console.log(err);
+    });
+}
+
+// Get Local JSON File Data
+function getJson() {
+  fetch('post.json')
+    .then(function(res) {
+      return res.json();
+    })
+
+    .then(function(data) {
+      console.log(data);
+      let output = '';
+      data.forEach(function(post) {
+        output += `<li>${post.title}</li>`;
+      });
+
+      document.getElementById('output').innerHTML = output;
+    })
+
+    .catch(function(err) {
+      console.log(err);
     });
 }
